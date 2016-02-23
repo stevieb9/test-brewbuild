ec90cc9da591c54e23e98965cd4bd3ffc2faff05
Steve Bertrand <steveb@cpan.org>
2016-02-23 15:27:06 -0700

    initial import

---
+++ lib/Test/BrewBuild.pm
@@ -0,0 +1,47 @@
+package Test::BrewBuild;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Test::BrewBuild - Cross-platform unit testing automation across numerous perl
+versions.
+
+=head1 SYNOPSIS
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-brewbuild at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-BrewBuild>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::BrewBuild
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Steve Bertrand.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Test::BrewBuild
