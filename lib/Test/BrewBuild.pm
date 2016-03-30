1058fb6f3ab5d86f32c66940d1a21aaa28de1476
Steve Bertrand <steveb@cpan.org>
2016-03-30 07:27:31 -0600

    attempt to fix ABORT issue (#50)

--- lib/Test/BrewBuild.pm
+++ lib/Test/BrewBuild.pm
@@ -99,6 +99,7 @@ sub instance_install {
 
     if ($self->{args}{version}->[0]){
         for my $version (@{ $self->{args}{version} }){
+            $version = "perl-$version" if ! $self->is_win;
             if (grep { $version eq $_ } @{ $perls_installed }){
                 $log->_6("$version is already installed... skipping");
                 next;
