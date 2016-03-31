61d38e9675e7f47130def35bbfca77c81c0b36e8
Steve Bertrand <steveb@cpan.org>
2016-03-31 17:31:09 +0000

    added File::Temp back to BrewBuild.pm

--- lib/Test/BrewBuild.pm
+++ lib/Test/BrewBuild.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 use File::Copy;
+use File::Temp;
 use Logging::Simple;
 use Plugin::Simple default => 'Test::BrewBuild::Plugin::DefaultExec';
 use Test::BrewBuild::BrewCommands;
