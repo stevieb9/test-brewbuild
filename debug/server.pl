9c47de8e44be62b8a01cb3444c13fa5a0773005e
Steve Bertrand <steveb@cpan.org>
2016-08-01 15:42:04 +0000

    updated debug/server

--- debug/server.pl
+++ debug/server.pl
@@ -2,12 +2,14 @@ use warnings;
 use strict;
 
 use IO::Socket::INET;
+use Proc::Background;
 use Storable;
 
 if (@ARGV && $ARGV[0] eq 'bg'){
-    system 1, 'perl', $0;
+    #system 1, 'perl', $0;
+    Proc::Background->new('perl', $0, 'fg');
 }
-else {
+if (@ARGV && $ARGV[0] eq 'fg') {
     my $sock = new IO::Socket::INET (
         LocalHost => '0.0.0.0',
         LocalPort => 7800,
@@ -24,12 +26,11 @@ else {
         $conn->recv($cmd, 1024);
 
         print "executing: $cmd\n";
-        my $ret = `$cmd`;
+        my $ret = `$cmd && exit`;
         print "return: $ret\n";
-        #$conn->send($ret);
         Storable::nstore_fd(\$ret, $conn);
 
-        shutdown($conn, 1);
+        #shutdown($conn, 1);
     }
 
     $sock->close;
